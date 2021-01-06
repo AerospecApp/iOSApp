ded4234cfb6db4ebdc62d8a0b7bdc563ac3858d8
Huy Nguyen <huynguyenviet@outlook.com>
2021-01-05 18:38:19 -0800

    Initial Commit

---
+++ Aerospec/AerospecApp.swift
@@ -0,0 +1,17 @@
+//
+//  AerospecApp.swift
+//  Aerospec
+//
+//  Created by Huy Nguyen on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct AerospecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
